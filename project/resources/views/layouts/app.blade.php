b4674ac56d86a4afa99a539980bbde62115ba120
ztarikul <ztarikulislam@gmail.com>
2021-09-08 00:41:17 +0600

    app blade edit

--- project/resources/views/layouts/app.blade.php
+++ project/resources/views/layouts/app.blade.php
@@ -54,18 +54,7 @@
                                 </li>
                             @endif
                             
-                            <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {{ Config::get('languages')[App::getLocale()] }}
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            @foreach (Config::get('languages') as $lang => $language)
-                                @if ($lang != App::getLocale())
-                                        <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"> {{$language}}</a>
-                                @endif
-                            @endforeach
-                            </div>
-                        </li>
+                           
                         @else
                         
                         
